e5d05ae45d06efb08b64d71105dcc65ea742bd27
Veyix <veyix@users.noreply.github.com>
2015-06-11 22:50:21 +0100

    Payroll SP parameters
    Added a parameter of employee name to the payment calculation SP to allow
    an employee to be specified or null to return all employees.
    
    Also added an index to the employee name to allow for optimised lookups on
    the employee name.
    
    Branch: feature/otb_task2

--- OnTheBeach/Otb.Interview.Database/Stored Procedures/Finance_RunAnnualPaymentCalculations.sql
+++ OnTheBeach/Otb.Interview.Database/Stored Procedures/Finance_RunAnnualPaymentCalculations.sql
@@ -1,12 +1,18 @@
-﻿create procedure Finance_RunAnnualPaymentCalculations
+﻿create procedure Finance_RunAnnualPaymentCalculations(
+	@EmployeeName nvarchar(255) = null
+)
 as
 begin
 
 	-- Collect the annual salaries of each employee in GBP
-	select e.id
-		 , e.name
-		 , round(s.annual_amount / c.conversion_factor, 2) annual_salary
+	-- Note: Filter by the employee name if we have one, otherwise default to retrieving all records.
+	select e.id employee_id
+		 , e.name employee_name
+		 , c.unit local_currency
+		 , convert(decimal(18, 2), s.annual_amount) local_annual_salary
+		 , convert(decimal(8, 2), round(s.annual_amount / c.conversion_factor, 2)) converted_annual_salary
 	from Employee e
 	inner join Salary s on s.employee_id = e.id
 	inner join Currency c on c.id = s.currency
+	where isnull(@EmployeeName, e.name) = e.name
 end
