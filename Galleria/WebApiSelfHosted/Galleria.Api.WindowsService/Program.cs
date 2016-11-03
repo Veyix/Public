e85a54af2d5feff47507bd23d05fd13deb6d2035
Samuel Slade <sam.slade@paystream.co.uk>
2016-11-03 20:03:14 +0000

    Setup windows service to host the API

---
+++ Galleria/WebApiSelfHosted/Galleria.Api.WindowsService/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceProcess;
+
+namespace Galleria.Api.WindowsService
+{
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        public static void Main()
+        {
+            ServiceBase.Run(
+                new[] { new UsersService() }
+            );
+        }
+    }
+}
---
+++ Galleria/WebApiSelfHosted/Galleria.Api.WindowsService/UsersService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.ServiceProcess;
+
+namespace Galleria.Api.WindowsService
+{
+    public partial class UsersService : ServiceBase
+    {
+        private IDisposable _api;
+
+        public UsersService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Startup the service using OWIN
+            _api = WebApp.Start<Startup>("http://localhost:60001/");
+        }
+
+        protected override void OnStop()
+        {
+            if (_api != null)
+            {
+                _api.Dispose();
+            }
+        }
+    }
+}
