673f37d7ea3b2f0085c4055475e5abe384736b86
Samuel Slade <sam.slade@paystream.co.uk>
2016-11-09 15:28:45 +0000

    Added the CompanyId column to the user profile table and setup the UserProfile in the object model project

---
+++ Galleria/TechTestPractice/Galleria.Profiles.DatabaseDeployer/Scripts/0002-UserProfile-CompanyId.column.sql
@@ -0,0 +1,16 @@
+﻿/*
+ * Description: Adds the CompanyId column to the UserProfile table.
+ *
+ * Author:		Samuel Slade
+ * Date:		09/11/2016
+ */
+IF NOT EXISTS (
+	SELECT TOP 1 1 FROM INFORMATION_SCHEMA.COLUMNS
+	WHERE [TABLE_SCHEMA] = N'dbo'
+	AND [TABLE_NAME] = N'UserProfile'
+	AND [COLUMN_NAME] = N'CompanyId'
+)
+BEGIN
+	ALTER TABLE [dbo].[UserProfile]
+	ADD [CompanyId] INT NOT NULL;
+END;
