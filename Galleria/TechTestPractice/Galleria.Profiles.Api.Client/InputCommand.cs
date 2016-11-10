1d44ebe88d945c2f3ba88cccb666fab2516d1c5f
Samuel Slade <sam.slade@paystream.co.uk>
2016-11-10 09:16:32 +0000

    Setup console application as HTTP client for service

---
+++ Galleria/TechTestPractice/Galleria.Profiles.Api.Client/InputCommand.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galleria.Profiles.Api.Client
+{
+    /// <summary>
+    /// A class that represents a command given through an input mechanism.
+    /// </summary>
+    public sealed class InputCommand
+    {
+        private const string COMMAND_EXIT = "exit";
+
+        private readonly Dictionary<string, string> _parameters =
+            new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+
+        private InputCommand(string commandText, bool isExitCommand)
+        {
+            CommandText = commandText;
+            IsExitCommand = isExitCommand;
+        }
+
+        /// <summary>
+        /// Gets the text given for the command.
+        /// </summary>
+        public string CommandText { get; }
+
+        /// <summary>
+        /// Gets a value that indicates whether the command is to exit the application.
+        /// </summary>
+        public bool IsExitCommand { get; }
+
+        private void AddParameter(string parameterName, string parameterValue)
+        {
+            _parameters.Add(parameterName, parameterValue);
+        }
+
+        /// <summary>
+        /// Gets the value of the specified parameter.
+        /// </summary>
+        /// <param name="parameterName">The name of the parameter for which to get the value.</param>
+        /// <returns>The value of the parameter; or null if the parameter does not exist.</returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="parameterName"/> is a null or empty string.</exception>
+        public string GetParameterValue(string parameterName)
+        {
+            if (String.IsNullOrWhiteSpace(parameterName)) throw new ArgumentException("The parameter name cannot be empty", nameof(parameterName));
+
+            string parameterValue;
+            if (!_parameters.TryGetValue(parameterName, out parameterValue))
+            {
+                return null;
+            }
+
+            return parameterValue;
+        }
+
+        /// <summary>
+        /// Creates an input command from the given text.
+        /// </summary>
+        /// <param name="inputText">The text from which to create the command.</param>
+        /// <returns>A new instance of the <see cref="InputCommand"/> class.</returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="inputText"/> is null or empty.</exception>
+        public static InputCommand Create(string inputText)
+        {
+            if (String.IsNullOrWhiteSpace(inputText)) throw new ArgumentException("The input cannot be empty", nameof(inputText));
+
+            string[] components = GetComponents(inputText);
+
+            string commandText = components[0];
+            bool isExitCommand = String.Equals(COMMAND_EXIT, commandText, StringComparison.InvariantCultureIgnoreCase);
+
+            var command = new InputCommand(commandText, isExitCommand);
+
+            // Parse all of the parameters from the components
+            for (int i = 1; i < components.Length; i++)
+            {
+                string component = components[i];
+                int separatorIndex = component.IndexOf('=');
+
+                string parameterName = component.Substring(0, separatorIndex);
+                string parameterValue = component.Substring(separatorIndex + 1);
+
+                command.AddParameter(parameterName, parameterValue);
+            }
+
+            return command;
+        }
+
+        private static string SanitizeInputText(string inputText)
+        {
+            return inputText.Trim(' ').ToLowerInvariant();
+        }
+
+        private static string[] GetComponents(string inputText)
+        {
+            var components = new List<string>();
+            var componentBuilder = new StringBuilder();
+
+            foreach (char character in inputText)
+            {
+                if (Char.IsWhiteSpace(character) && componentBuilder.Length > 0)
+                {
+                    // We have our first whitespace character, so add the contents of the
+                    // builder to the component list and reset it until we find our next
+                    // non-whitespace character
+                    components.Add(componentBuilder.ToString());
+                    componentBuilder.Clear();
+                }
+                else
+                {
+                    componentBuilder.Append(character);
+                }
+            }
+
+            if (componentBuilder.Length > 0)
+            {
+                // We have content left in the builder, so add it as the final component
+                components.Add(componentBuilder.ToString());
+            }
+
+            return components.ToArray();
+        }
+    }
+}
---
+++ Galleria/TechTestPractice/Galleria.Profiles.Api.Client/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Http;
+
+namespace Galleria.Profiles.Api.Client
+{
+    /// <summary>
+    /// A class that provides the main entry point for the application.
+    /// </summary>
+    public sealed class Program
+    {
+        private readonly IInputReceiver _inputReceiver;
+        private readonly HttpClient _client;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Program"/> class.
+        /// </summary>
+        /// <param name="inputReceiver">A receiver of input commands.</param>
+        /// <param name="apiAddress">The URL to the hosted API.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="inputReceiver"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="apiAddress"/> is null or empty.</exception>
+        public Program(IInputReceiver inputReceiver, string apiAddress)
+        {
+            if (inputReceiver == null) throw new ArgumentNullException(nameof(inputReceiver));
+            if (String.IsNullOrWhiteSpace(apiAddress)) throw new ArgumentException("The API address cannot be empty", nameof(apiAddress));
+
+            _inputReceiver = inputReceiver;
+
+            // Setup and initialize the API client
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri(apiAddress);
+        }
+
+        private void Run()
+        {
+            InputCommand command;
+
+            do
+            {
+                command = _inputReceiver.GetNextInputCommand();
+
+                if (!command.IsExitCommand)
+                {
+                    HandleCommand(command);
+                }
+            }
+            while (command != null && !command.IsExitCommand);
+        }
+
+        private void HandleCommand(InputCommand command)
+        {
+            switch (command.CommandText)
+            {
+                case "getall":
+                    ShowAllUserProfiles();
+                    break;
+
+                case "get":
+                    {
+                        string userIdString = command.GetParameterValue("userId");
+                        string companyIdString = command.GetParameterValue("companyId");
+
+                        if (!String.IsNullOrWhiteSpace(userIdString))
+                        {
+                            int userId;
+                            if (Int32.TryParse(userIdString, out userId))
+                            {
+                                ShowSpecificUserProfile(userId);
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                Console.WriteLine($"{userIdString} is not a valid value for the 'userId' parameter");
+                            }
+                        }
+                        else if (!String.IsNullOrWhiteSpace(companyIdString))
+                        {
+                            int companyId;
+                            if (Int32.TryParse(companyIdString, out companyId))
+                            {
+                                ShowUserProfilesByCompany(companyId);
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                Console.WriteLine($"{companyIdString} is not a valid value for the 'companyId' parameter");
+                            }
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine("You must specify either the 'userId' or 'companyId' parameter for the get command");
+                        }
+                    }
+                    break;
+
+                default:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"{command.CommandText} is not a recognised command");
+                    break;
+            }
+
+            Console.ResetColor();
+        }
+
+        private void ShowAllUserProfiles()
+        {
+            var task = _client.GetStringAsync("api/users");
+            task.Wait();
+
+            string result = task.Result;
+            // TODO: Parse object from JSON.
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(result);
+            Console.ResetColor();
+        }
+
+        private void ShowUserProfilesByCompany(int companyId)
+        {
+            var task = _client.GetStringAsync($"api/company/{companyId}/users");
+            task.Wait();
+
+            string result = task.Result;
+            // TODO: Parse object from JSON.
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(result);
+            Console.ResetColor();
+        }
+
+        private void ShowSpecificUserProfile(int userId)
+        {
+            var task = _client.GetStringAsync($"api/users/{userId}");
+            task.Wait();
+
+            string result = task.Result;
+            // TODO: Parse object from JSON.
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(result);
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        public static void Main()
+        {
+            var receiver = new ConsoleInputReceiver();
+
+            try
+            {
+                Console.WriteLine("Starting client...");
+
+                var program = new Program(receiver, GetApiAddress());
+                program.Run();
+            }
+            catch (Exception exception)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(exception.Message);
+            }
+            finally
+            {
+                Console.ResetColor();
+            }
+        }
+
+        private static string GetApiAddress()
+        {
+            return ConfigurationManager.AppSettings["ApiAddress"];
+        }
+    }
+}
