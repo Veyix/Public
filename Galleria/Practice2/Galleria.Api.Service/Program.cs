bd38bab231e14270f361642b8dfa3000f9d59307
Samuel Slade <sam.slade@paystream.co.uk>
2016-11-20 19:36:44 +0000

    Practice 2 - DBUp

--- Galleria/Practice2/Galleria.Api.Service/Program.cs
+++ Galleria/Practice2/Galleria.Api.Service/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DbUp;
+using System;
 using System.Configuration;
 using System.ServiceProcess;
 
@@ -11,6 +12,25 @@ namespace Galleria.Api.Service
         /// </summary>
         public static void Main()
         {
+            try
+            {
+                Run();
+            }
+            catch (Exception exception)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(exception.Message);
+            }
+            finally
+            {
+                Console.ResetColor();
+            }
+        }
+
+        private static void Run()
+        {
+            UpdateDatabase();
+
             string hostAddress = ConfigurationManager.AppSettings["HostAddress"];
             var service = new HostService(hostAddress);
 
@@ -22,6 +42,42 @@ namespace Galleria.Api.Service
             {
                 ServiceBase.Run(service);
             }
+        }
+
+        private static void UpdateDatabase()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Galleria"].ConnectionString;
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            Console.WriteLine("Using connection string " + connectionString);
+            var builder = DeployChanges.To.SqlDatabase(connectionString)
+                .WithScriptsEmbeddedInAssembly(typeof(Program).Assembly)
+                .Build();
+
+            if (!builder.IsUpgradeRequired())
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Database already up to date");
+                Console.ResetColor();
+
+                return;
+            }
+
+            Console.WriteLine("Upgrading database...");
+
+            var result = builder.PerformUpgrade();
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error.Message);
+                Console.ResetColor();
+
+                return;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Database upgraded successfully!");
+            Console.ResetColor();
         }
     }
 }
