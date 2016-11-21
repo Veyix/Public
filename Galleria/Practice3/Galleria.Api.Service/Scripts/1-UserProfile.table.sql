e4bcf59e1c73d504f951f3863bc16d6a91aff761
Samuel Slade <sam.slade@paystream.co.uk>
2016-11-21 11:41:13 +0000

    Practice 3 - Backend

---
+++ Galleria/Practice3/Galleria.Api.Service/Scripts/1-UserProfile.table.sql
@@ -0,0 +1,12 @@
+﻿create table dbo.UserProfile (
+	Id int not null identity (1, 1),
+	CompanyId int not null,
+	Title varchar(50) not null,
+	Forename varchar(100) not null,
+	Surname varchar(100) not null,
+	DateOfBirth date not null
+);
+
+alter table dbo.UserProfile
+add constraint [PK_dbo-UserProfile:Id]
+	primary key clustered ([Id] asc);
