0087aee5fed149cbcc016d5113424fe56134af59
Samuel Slade <sam.slade@paystream.co.uk>
2016-11-22 09:56:15 +0000

    Added authentication and authorization to practice 3

--- Galleria/Practice3/Galleria.Api.Service/ServiceStartup.cs
+++ Galleria/Practice3/Galleria.Api.Service/ServiceStartup.cs
@@ -1,4 +1,6 @@
-﻿using Owin;
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
 using System.Web.Http;
 
 namespace Galleria.Api.Service
@@ -10,6 +12,15 @@ namespace Galleria.Api.Service
             var config = new HttpConfiguration();
             config.MapHttpAttributeRoutes();
 
+            builder.UseOAuthAuthorizationServer(
+                new OAuthAuthorizationServerOptions()
+                {
+                    TokenEndpointPath = new PathString("/api/token"),
+                    Provider = new CredentialVerificationProvider()
+                }
+            );
+
+            builder.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
             builder.UseWebApi(config);
         }
     }
