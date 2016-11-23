7ff735e6767458b4f07f169ffbe31e7bd9da5d82
Samuel Slade <sam.slade@paystream.co.uk>
2016-11-23 14:49:17 +0000

    Autofac

---
+++ Galleria/FinalTechTest/Galleria.Api.Service/ServiceModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+
+namespace Galleria.Api.Service
+{
+    /// <summary>
+    /// A class that handles registration of components for the service.
+    /// </summary>
+    public sealed class ServiceModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+        }
+    }
+}
--- Galleria/FinalTechTest/Galleria.Api.Service/ServiceStartup.cs
+++ Galleria/FinalTechTest/Galleria.Api.Service/ServiceStartup.cs
@@ -1,4 +1,6 @@
-﻿using Owin;
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using Owin;
 using System.Web.Http;
 
 namespace Galleria.Api.Service
@@ -17,7 +19,20 @@ namespace Galleria.Api.Service
             var config = new HttpConfiguration();
             config.MapHttpAttributeRoutes();
 
+            var container = CreateContainer();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            builder.UseAutofacMiddleware(container);
+            builder.UseAutofacWebApi(config);
             builder.UseWebApi(config);
+        }
+
+        private IContainer CreateContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(GetType().Assembly);
+
+            return builder.Build();
         }
     }
 }
