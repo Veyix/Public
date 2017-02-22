c9509e4708d7e061546c41d80347cf7c80ee723c
Samuel Slade <samuel.pjm.slade@googlemail.com>
2017-02-22 13:31:40 +0000

    Added parse logic to the tool selection command

--- RoBAT/Robat.SpindleFileConverter/Robat.SpindleFileConverter/ToolSelectionCommand.cs
+++ RoBAT/Robat.SpindleFileConverter/Robat.SpindleFileConverter/ToolSelectionCommand.cs
@@ -1,10 +1,19 @@
-﻿namespace Robat.SpindleFileConverter
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Robat.SpindleFileConverter
 {
     public sealed class ToolSelectionCommand : ICommand
     {
-        public ToolSelectionCommand(string commandText)
+        private readonly int _toolNumber;
+        private readonly int? _headNumber;
+
+        public ToolSelectionCommand(string commandText, int toolNumber, int? headNumber = null)
         {
             CommandText = commandText;
+
+            _toolNumber = toolNumber;
+            _headNumber = headNumber;
         }
 
         public string CommandText { get; }
@@ -17,8 +26,51 @@
 
         public static ICommand FromCommandText(string commandText)
         {
-            // TODO: Create properly.
-            return new ToolSelectionCommand(commandText);
+            int toolNumber = GetToolNumber(commandText);
+            int? headNumber = GetHeadNumber(commandText);
+
+            return new ToolSelectionCommand(commandText, toolNumber, headNumber);
+        }
+
+        private static int GetToolNumber(string commandText)
+        {
+            const string pattern = @"^T([0-9])";
+            var match = Regex.Match(commandText, pattern);
+
+            if (match.Groups.Count < 2)
+            {
+                throw new FormatException($"The command '{commandText}' does not contain a valid tool number");
+            }
+
+            int toolNumber;
+            if (!Int32.TryParse(match.Groups[1].Value, out toolNumber))
+            {
+                throw new FormatException($"The command '{commandText}' does not contain a valid tool number");
+            }
+
+            return toolNumber;
+        }
+
+        private static int? GetHeadNumber(string commandText)
+        {
+            const string pattern = @"^T[0-9]\sH([0-9])$";
+            var match = Regex.Match(commandText, pattern);
+
+            if (match.Groups.Count < 2)
+            {
+                // No head number specified
+                return null;
+            }
+
+            string headNumberString = match.Groups[1].Value;
+
+            int headNumber;
+            if (!Int32.TryParse(headNumberString, out headNumber))
+            {
+                throw new FormatException($"The head number '{headNumberString}' is not a valid head number");
+            }
+
+            return headNumber;
         }
     }
 }
