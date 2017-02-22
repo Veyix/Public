464499f288cfcf0018e9d1a028f79d77212f6c25
Samuel Slade <samuel.pjm.slade@googlemail.com>
2017-02-22 13:37:50 +0000

    Added parse logic for the stop drill command

--- RoBAT/Robat.SpindleFileConverter/Robat.SpindleFileConverter/StopDrillCommand.cs
+++ RoBAT/Robat.SpindleFileConverter/Robat.SpindleFileConverter/StopDrillCommand.cs
@@ -1,10 +1,17 @@
-﻿namespace Robat.SpindleFileConverter
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Robat.SpindleFileConverter
 {
     public sealed class StopDrillCommand : ICommand
     {
-        public StopDrillCommand(string commandText)
+        private readonly int? _headNumber;
+
+        public StopDrillCommand(string commandText, int? headNumber)
         {
             CommandText = commandText;
+
+            _headNumber = headNumber;
         }
 
         public string CommandText { get; }
@@ -17,8 +24,30 @@
 
         public static ICommand FromCommandText(string commandText)
         {
-            // TODO: Create properly
-            return new StopDrillCommand(commandText);
+            int? headNumber = GetHeadNumber(commandText);
+            return new StopDrillCommand(commandText, headNumber);
+        }
+
+        private static int? GetHeadNumber(string commandText)
+        {
+            const string pattern = @"^M05(\sH([0-9]))*$";
+            var match = Regex.Match(commandText, pattern);
+
+            if (match.Groups.Count < 3)
+            {
+                // No head number specified
+                return null;
+            }
+
+            string headNumberString = match.Groups[2].Value;
+            int headNumber;
+
+            if (!Int32.TryParse(headNumberString, out headNumber))
+            {
+                throw new FormatException($"The head number '{headNumberString}' is not a valid value");
+            }
+
+            return headNumber;
         }
     }
 }
