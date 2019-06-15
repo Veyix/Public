9f13d4e2f8a33763ec1d181bbe6deb9076458822
Samuel Slade <samuel.pjm.slade@live.co.uk>
2019-06-15 14:09:01 +0100

    Included the 3 submissions

---
+++ EqualExperts/dotnet/run-on-linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo Restoring packages...
+dotnet restore ./FizzBuzz/FizzBuzz.csproj
+dotnet restore ./FizzBuzz.Tests/FizzBuzz.Tests.csproj
+
+echo Building the code...
+dotnet build -c Release --no-restore ./FizzBuzz/FizzBuzz.csproj
+dotnet build -c Debug --no-restore ./FizzBuzz.Tests/FizzBuzz.Tests.csproj
+
+echo Running tests...
+dotnet test -c Debug --no-restore --no-build ./FizzBuzz.Tests/FizzBuzz.Tests.csproj
+
+echo Running the program...
+dotnet run --no-restore --no-build --project FizzBuzz/FizzBuzz.csproj
+
+echo Done!
