dd1b15d9d533f721d805d84d7468bb1203656b6a
Samuel Slade <samuel.pjm.slade@live.co.uk>
2019-06-15 11:12:51 +0100

    Handled lucky number 3

--- EqualExperts/dotnet/FizzBuzz.Tests/FizzerTests.cs
+++ EqualExperts/dotnet/FizzBuzz.Tests/FizzerTests.cs
@@ -6,10 +6,10 @@ namespace FizzBuzz.Tests
     public class FizzerTests
     {
         [Theory]
-        [InlineData("1 2 fizz 4 buzz fizz 7 8 fizz buzz", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)]
-        [InlineData("fizzbuzz fizzbuzz fizzbuzz", 15, 30, 45)]
-        [InlineData("fizz fizz fizz fizz fizz", 3, 6, 9, 12, 18)]
-        [InlineData("buzz buzz buzz buzz buzz", 5, 10, 20, 25, 35)]
+        [InlineData("1 2 lucky 4 buzz fizz 7 8 fizz buzz", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)]
+        [InlineData("fizzbuzz lucky fizzbuzz", 15, 30, 45)]
+        [InlineData("lucky fizz fizz fizz fizz", 3, 6, 9, 12, 18)]
+        [InlineData("buzz buzz buzz buzz lucky", 5, 10, 20, 25, 35)]
         public void GivenASetOfNumbers_WhenCallingTheFizzer_ThenTheExpectedOutputShouldBeProduced(
             string expectedOutput, params int[] numbers)
         {
--- EqualExperts/dotnet/FizzBuzz/Fizzer.cs
+++ EqualExperts/dotnet/FizzBuzz/Fizzer.cs
@@ -10,13 +10,33 @@ namespace FizzBuzz
         {
             _outputter = outputter ?? throw new ArgumentNullException(nameof(outputter));
         }
-        
+
         public void Run(params int[] numbers)
         {
             for (int i = 0; i < numbers.Length; i++)
             {
                 int number = numbers[i];
 
+                ProcessNumber(number);
+
+                if (!IsLastNumber(i, numbers.Length))
+                {
+                    _outputter.Output(" ");
+                }
+            }
+        }
+
+        private static bool IsLastNumber(int currentIndex, int numberCount) =>
+            currentIndex == numberCount - 1;
+
+        private void ProcessNumber(int number)
+        {
+            if (DoesNumberContainThree(number))
+            {
+                _outputter.Output("lucky");
+            }
+            else
+            {
                 bool isMultipleOfThree = IsMultipleOfThree(number);
                 bool isMultipleOfFive = IsMultipleOfFive(number);
 
@@ -36,14 +56,10 @@ namespace FizzBuzz
                 {
                     _outputter.Output(number.ToString());
                 }
-
-                if (i < numbers.Length - 1)
-                {
-                    _outputter.Output(" ");
-                }
             }
         }
 
+        private static bool DoesNumberContainThree(int number) => number.ToString().Contains("3");
         private static bool IsMultipleOfThree(int number) => number % 3 == 0;
         private static bool IsMultipleOfFive(int number) => number % 5 == 0;
     }
