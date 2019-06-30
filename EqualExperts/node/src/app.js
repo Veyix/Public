4fe0b38dd9b3ec3b18ababf7afee34ed1ae51ef4
Samuel Slade <samuel.pjm.slade@googlemail.com>
2019-06-30 17:23:04 +0100

    Fixed app input numbers

--- EqualExperts/node/src/app.js
+++ EqualExperts/node/src/app.js
@@ -1,6 +1,6 @@
 const fizzer = require("./fizzer")
 
-const numbers = [Array(21).keys()].splice(1)
+const numbers = [...Array(21).keys()].splice(1)
 const output = fizzer(numbers)
 
 console.log(output)
